7871e33b9ad0c85ffed4541a5cd5561ab61a2409
Lionel <lionelteojiarong@gmail.com>
2020-04-10 16:01:39 +0800

    changed 'Back to login' to use Navigator.pop() instead of routing to new page

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -12,7 +12,6 @@ import 'package:taskme/screens/home_screen.dart';
 class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     final model = Provider.of<HomeModel>(context);
     final size = MediaQuery.of(context).size;
     bool keyboardOpen = MediaQuery.of(context).viewInsets.bottom > 0;
@@ -115,44 +114,45 @@ class RegisterScreen extends StatelessWidget {
                   pageRoute: '/home',
                 ),
                 SizedBox(height: 10),
-            Material(
-              child: Ink(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topRight,
-                      end: Alignment(0.1,6.2) ,
-                      colors: true ? [Global.white,Global.white] : [ Global.lesshotpink, Global.hotpink]
-                  ),
-
-                  color: true ? Global.white : Global.lesshotpink,
-                  borderRadius: BorderRadius.circular(10),
-                  border: true ? Border.all(
-                    color: Global.hotpink,
-                    width: 1.0,
-                  )
-                      : Border.fromBorderSide(BorderSide.none),
-                ),
-                child: InkWell(
-                  onTap: (){
-                    Navigator.pop(context);
-                  },
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: Container(
-                    height: 60.0,
-                    child: Center(
-                      child: Text(
-                        'Return to login',
-                        style: TextStyle(
-                          color: true? Global.hotpink : Global.white,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16.0,
+                Material(
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topRight,
+                          end: Alignment(0.1, 6.2),
+                          colors: true
+                              ? [Global.white, Global.white]
+                              : [Global.lesshotpink, Global.hotpink]),
+                      color: true ? Global.white : Global.lesshotpink,
+                      borderRadius: BorderRadius.circular(10),
+                      border: true
+                          ? Border.all(
+                              color: Global.hotpink,
+                              width: 1.0,
+                            )
+                          : Border.fromBorderSide(BorderSide.none),
+                    ),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Container(
+                        height: 60.0,
+                        child: Center(
+                          child: Text(
+                            'Return to login',
+                            style: TextStyle(
+                              color: true ? Global.hotpink : Global.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16.0,
+                            ),
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ),
               ],
             ),
           ),
