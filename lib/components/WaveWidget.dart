fa92227760a183bdf0556a0ae6a3d6cbae767997
Lionel <lionelteojiarong@gmail.com>
2020-04-10 09:34:49 +0800

    Implemented dispose() for animation controller

--- lib/components/WaveWidget.dart
+++ lib/components/WaveWidget.dart
@@ -46,12 +46,6 @@ class _WaveWidgetState extends State<WaveWidget> with TickerProviderStateMixin {
   }
 
   @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
       animation: animationController,
@@ -66,4 +60,11 @@ class _WaveWidgetState extends State<WaveWidget> with TickerProviderStateMixin {
       },
     );
   }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    animationController.dispose();
+    super.dispose();
+  }
 }
+
